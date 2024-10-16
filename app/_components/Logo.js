2a7f479b16e41a1a6727be96fe60a0bc57b2e382
acharayaP03 <acharyaP03@gmail.com>
2024-10-16 14:43:59 +1100

    Use provided Image from next

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,11 @@
+import Image from 'next/image';
+import Link from 'next/link';
 function Logo() {
 	return (
-		<a href='/' className='flex items-center gap-4 z-10'>
-			<img src='/logo.png' height='60' width='60' alt='The Nature Cabin logo' />
-			<span className='text-xl font-semibold text-primary-100'>The Nature's Cabin</span>
-		</a>
+		<Link href='/' className='flex items-center gap-4 z-10'>
+			<Image src='/logo.png' height='60' width='60' alt='The Nature Cabin logo' />
+			<span className='text-xl font-semibold text-primary-100'>The Nature Cabins</span>
+		</Link>
 	);
 }
 
