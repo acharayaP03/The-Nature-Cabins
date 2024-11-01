f1de2d33edd867a288eb2f17ab70bedae74eff9c
Prabhakar acharya <acharyap03@gmail.com>
2024-11-01 21:18:35 +1100

    default export

---
+++ app/_components/cabins/index.js
@@ -0,0 +1,8 @@
+import DateSelector from './DateSelector';
+import ReservationForm from './ReservationForm';
+import CabinFilter from './CabinFilter';
+import CabinList from './CabinList';
+import CabinCard from './CabinCard';
+import TextExpander from './TextExpander';
+
+export { DateSelector, ReservationForm, CabinFilter, CabinList, CabinCard, TextExpander };
