9ab306b259557c0737095cc92c89b172eb7d1f79
Prabhakar acharya <acharyap03@gmail.com>
2024-12-22 18:23:50 +1100

    create user and update session with guest id once the sign in is success

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { getGuest, createGuest } from './data-service';
 
 const authConfig = {
 	providers: [
@@ -13,6 +14,34 @@ const authConfig = {
 	callbacks: {
 		authorized({ auth, request }) {
 			return !!auth?.user;
+		},
+		/**
+		 * @description
+		 * this willl be called when the user sign in,
+		 * guest will be checked their email exist in supabase, if not it will be created
+		 * supabase will return the user object if it exist, if not it will return null
+		 */
+		async signIn({ user }) {
+			try {
+				const existingGuest = await getGuest(user.email);
+				if (!existingGuest) {
+					await createGuest({ email: user.email, full_name: user.name });
+				}
+				return true;
+			} catch {
+				return false;
+			}
+		},
+		/**
+		 * once the new user is created, the session needs to be updated with the new guestId,
+		 * so that we can show their reservations
+		 * @param {*} param0
+		 * @returns updated session
+		 */
+		async session({ session }) {
+			const guest = await getGuest(session.user.email);
+			session.user.guestId = guest.id;
+			return session;
 		},
 	},
 	pages: {
