532d067a9bafb6ff45a9b1f17a999c5166cddea3
Prabhakar acharya <acharyap03@gmail.com>
2024-12-25 19:51:41 +1100

    update user input to receive name props so that we can send formdata to server

--- app/_components/account/profile/UpdateProfileForm.js
+++ app/_components/account/profile/UpdateProfileForm.js
@@ -1,37 +1,66 @@
-'use client';
+"use client";
 
-import { DefaultButton } from '@/app/_sharedUI/DefaultButton';
-import FormInput from '@/app/_sharedUI/FormInput';
+import { updateGuestProfile } from "@/app/_lib/guest.actions";
+import { DefaultButton } from "@/app/_sharedUI/DefaultButton";
+import FormInput from "@/app/_sharedUI/FormInput";
 
 export default function UpdateProfileForm({ guest, children }) {
-	console.log(guest);
-	const { full_name: fullName, email, national_id: nationalID, country_flag: countryFlag } = guest;
+  const {
+    full_name: fullName,
+    email,
+    national_id: nationalID,
+    country_flag: countryFlag,
+  } = guest;
 
-	return (
-		<form className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'>
-			<div className='space-y-2'>
-				<FormInput label='Full name' type='text' defaultValue={fullName} disabled />
-			</div>
+  return (
+    <form
+      action={updateGuestProfile}
+      className="flex flex-col gap-6 bg-primary-900 px-12 py-8 text-lg"
+    >
+      <div className="space-y-2">
+        <FormInput
+          label="Full name"
+          type="text"
+          defaultValue={fullName}
+          disabled
+          name="fullName"
+        />
+      </div>
 
-			<div className='space-y-2'>
-				<FormInput label='Email address' type='email' defaultValue={email} disabled />
-			</div>
+      <div className="space-y-2">
+        <FormInput
+          label="Email address"
+          type="email"
+          defaultValue={email}
+          disabled
+          name="email"
+        />
+      </div>
 
-			<div className='space-y-2'>
-				<div className='flex items-center justify-between'>
-					<label htmlFor='nationality'>Where are you from?</label>
-					<img src={countryFlag} alt='Country flag' className='h-5 rounded-sm' />
-				</div>
-				{children}
-			</div>
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <label htmlFor="nationality">Where are you from?</label>
+          <img
+            src={countryFlag}
+            alt="Country flag"
+            className="h-5 rounded-sm"
+          />
+        </div>
+        {children}
+      </div>
 
-			<div className='space-y-2'>
-				<FormInput label='National ID number' type='text' defaultValue={nationalID} />
-			</div>
+      <div className="space-y-2">
+        <FormInput
+          label="National ID number"
+          type="text"
+          defaultValue={nationalID}
+          name="nationalID"
+        />
+      </div>
 
-			<div className='flex justify-end items-center gap-6'>
-				<DefaultButton label='Update profile' />
-			</div>
-		</form>
-	);
+      <div className="flex items-center justify-end gap-6">
+        <DefaultButton label="Update profile" />
+      </div>
+    </form>
+  );
 }
--- app/_sharedUI/FormInput.js
+++ app/_sharedUI/FormInput.js
@@ -1,13 +1,20 @@
-export default function FormInput({ label, type, defaultValue, disabled }) {
-	return (
-		<>
-			<label>{label}</label>
-			<input
-				type={type}
-				disabled={disabled}
-				defaultValue={defaultValue}
-				className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
-			/>
-		</>
-	);
+export default function FormInput({
+  label,
+  type,
+  defaultValue,
+  disabled,
+  name,
+}) {
+  return (
+    <>
+      <label>{label}</label>
+      <input
+        type={type}
+        disabled={disabled}
+        name={name}
+        defaultValue={defaultValue}
+        className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+      />
+    </>
+  );
 }
