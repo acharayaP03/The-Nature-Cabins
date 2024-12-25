d2e7ce6cb28aad0dec6217039b9d3e704cc147cc
Prabhakar acharya <acharyap03@gmail.com>
2024-12-26 08:53:21 +1100

    revalidate cache after update on user profile

--- app/_lib/guest.actions.js
+++ app/_lib/guest.actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth } from "./auth";
 import supabase from "./supabase";
 
@@ -11,6 +12,7 @@ export async function updateGuestProfile(formData) {
 
   const [nationality, countryFlag] = formData.get("nationality").split("%");
 
+  const nationalID = formData.get("nationalID");
   if (!/^[a-zA-Z-0-9]{6,12}$/.test(nationalID))
     throw new Error(
       "Invalid national ID number. Please provide a valid ID number.",
@@ -32,4 +34,7 @@ export async function updateGuestProfile(formData) {
     console.error(error);
     throw new Error("Guest could not be updated");
   }
+
+  // revalidate cache after updating the guest profile
+  revalidatePath("/account/profile");
 }
