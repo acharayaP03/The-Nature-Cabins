b1a5caf829d6a9f75a700971e43d70941ffd117e
Prabhakar acharya <acharyap03@gmail.com>
2024-12-25 19:11:04 +1100

    prefill form with supabase data

--- app/_components/account/profile/UpdateProfileForm.js
+++ app/_components/account/profile/UpdateProfileForm.js
@@ -1,25 +1,20 @@
 'use client';
-export default function UpdateProfileForm({ children }) {
-	// CHANGE
-	const countryFlag = 'pt.jpg';
-	const nationality = 'portugal';
+
+import { DefaultButton } from '@/app/_sharedUI/DefaultButton';
+import FormInput from '@/app/_sharedUI/FormInput';
+
+export default function UpdateProfileForm({ guest, children }) {
+	console.log(guest);
+	const { full_name: fullName, email, national_id: nationalID, country_flag: countryFlag } = guest;
 
 	return (
 		<form className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'>
 			<div className='space-y-2'>
-				<label>Full name</label>
-				<input
-					disabled
-					className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
-				/>
+				<FormInput label='Full name' type='text' defaultValue={fullName} disabled />
 			</div>
 
 			<div className='space-y-2'>
-				<label>Email address</label>
-				<input
-					disabled
-					className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
-				/>
+				<FormInput label='Email address' type='email' defaultValue={email} disabled />
 			</div>
 
 			<div className='space-y-2'>
@@ -27,22 +22,15 @@ export default function UpdateProfileForm({ children }) {
 					<label htmlFor='nationality'>Where are you from?</label>
 					<img src={countryFlag} alt='Country flag' className='h-5 rounded-sm' />
 				</div>
-
 				{children}
 			</div>
 
 			<div className='space-y-2'>
-				<label htmlFor='nationalID'>National ID number</label>
-				<input
-					name='nationalID'
-					className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm'
-				/>
+				<FormInput label='National ID number' type='text' defaultValue={nationalID} />
 			</div>
 
 			<div className='flex justify-end items-center gap-6'>
-				<button className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
-					Update profile
-				</button>
+				<DefaultButton label='Update profile' />
 			</div>
 		</form>
 	);
