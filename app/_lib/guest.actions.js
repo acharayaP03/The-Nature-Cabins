c4177cc134566d8acbcc6f1765c903fb1a50dbb6
Prabhakar acharya <acharyap03@gmail.com>
2024-12-26 17:23:13 +1100

    resuable error handler

--- app/_lib/guest.actions.js
+++ app/_lib/guest.actions.js
@@ -3,18 +3,18 @@
 import { revalidatePath } from "next/cache";
 import { auth } from "./auth";
 import supabase from "./supabase";
+import { errorHandler } from "./utils";
 
 export async function updateGuestProfile(formData) {
   const session = await auth();
 
-  if (!session)
-    throw new Error("You must be logged in to update your profile.");
+  if (!session) errorHandler("You must be logged in to update your profile.");
 
   const [nationality, countryFlag] = formData.get("nationality").split("%");
 
   const nationalID = formData.get("nationalID");
   if (!/^[a-zA-Z-0-9]{6,12}$/.test(nationalID))
-    throw new Error(
+    errorHandler(
       "Invalid national ID number. Please provide a valid ID number.",
     );
   const updateData = {
@@ -23,8 +23,6 @@ export async function updateGuestProfile(formData) {
     national_id: nationalID,
   };
 
-  console.log("Profile updated!", updateData);
-
   const { error } = await supabase
     .from("guests")
     .update(updateData)
@@ -32,7 +30,7 @@ export async function updateGuestProfile(formData) {
 
   if (error) {
     console.error(error);
-    throw new Error("Guest could not be updated");
+    errorHandler("Guest could not be updated");
   }
 
   // revalidate cache after updating the guest profile
